64537f8b0ab94a3309d0c678f541f0205db0665d
JonathanRicardo <jonricfel@gmail.com>
2020-11-02 15:32:40 -0300

    Initial commit

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,18 @@
+import { MikroORM } from "@mikro-orm/core";
+import { Post } from "./entities/Post";
+import { __prod__ } from "./constants";
+import path from "path";
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/,
+  },
+  entities: [Post],
+  dbName: "postgres",
+  type: "postgresql",
+  user: "postgres",
+  password: "postgresql",
+  host: "192.168.99.100",
+  debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
