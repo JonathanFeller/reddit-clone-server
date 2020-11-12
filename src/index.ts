ea97f8b2f2a70f85fe916aa786d88aab80c409a2
JonathanRicardo <jonricfel@gmail.com>
2020-11-11 23:13:19 -0300

    Added posts mock data

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import cors from "cors";
 import { createConnection } from "typeorm";
 import { Post } from "./entities/Post";
 import { User } from "./entities/User";
+import path from "path";
 
 const main = async () => {
   const conn = await createConnection({
@@ -24,9 +25,10 @@ const main = async () => {
     host: "192.168.99.100",
     logging: !__prod__,
     synchronize: true,
+    migrations: [path.join(__dirname, "./migrations/*")],
     entities: [Post, User],
   });
-  conn.driver;
+  conn.runMigrations();
 
   const app = express();
 
