9f6c7d8862a8291e25d830ead1b112b8a93c69f7
JonathanRicardo <jonricfel@gmail.com>
2020-11-17 22:14:17 -0300

    Added vote entity

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -5,10 +5,12 @@ import {
   CreateDateColumn,
   Entity,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from "typeorm";
 import { User } from "./User";
+import { Vote } from "./Vote";
 
 @ObjectType()
 @Entity()
@@ -44,4 +46,7 @@ export class Post extends BaseEntity {
   @Field()
   @ManyToOne(() => User, (user) => user.posts)
   creator: User;
+
+  @OneToMany(() => Vote, (vote) => vote.post)
+  votes: Vote[];
 }
